f1545af98b6d7efef8de0de2e353df43f6015714
JAYBORICHA <jayboricha707@gmail.com>
2024-02-14 17:18:14 +0530

    Added console

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -6,16 +6,8 @@ type TrackOptions = {
     persist?: boolean
 }
 
-type AnalyticsArgs = {
-    retention?: number
-}
 
 export class Analytics {
-    private retention: number = 60 * 60 * 24 * 7
-
-    constructor(opts?: AnalyticsArgs) {
-        if (opts?.retention) this.retention = opts.retention
-    }
 
     async track(namespace: string, event: object = {}, opts?: TrackOptions) {
 
@@ -29,7 +21,6 @@ export class Analytics {
         console.log(key)
         console.log(JSON.stringify(event));
         await redis.hincrby(key, JSON.stringify(event), 1)
-        if (!opts?.persist) await redis.expire(key, this.retention)
 
     }
 
