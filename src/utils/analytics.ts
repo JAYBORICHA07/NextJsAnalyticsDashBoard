19b6baf217607cf7813d127f642d69a197c09e90
JAYBORICHA <jayboricha707@gmail.com>
2024-02-14 12:40:20 +0530

    Added console

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -26,7 +26,7 @@ export class Analytics {
         }
 
         //db call to persist this event
-        console.log("first")
+        console.log(key)
         console.log(JSON.stringify(event));
         await redis.hincrby(key, JSON.stringify(event), 1).then((res)=>(console.log(res)))
         if (!opts?.persist) await redis.expire(key, this.retention)
